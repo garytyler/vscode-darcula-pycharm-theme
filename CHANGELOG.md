6281b74897bf727dedcc177e8c1a9e0ef07b01ce
danields <danields761@gmail.com>
2017-10-25 14:42:02 +0300

    Change  decorator colorization rules, removing font bolding and colorization of some arguments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## [0.1.2]
+
+- Change  decorator colorization rules, removing font bolding and colorization of some arguments
+
 ## [0.1.1]
 
 - Fixes self styling in method scope
---
+++ test/snippets/decorator.py
@@ -0,0 +1,24 @@
+# pylint: disable-all
+
+def decorator():
+    pass
+
+
+def argued_decorator(arg):
+    pass
+
+
+class A:
+    @decorator
+    def meth(self):
+        pass
+
+    @argued_decorator(int, 100, 'test', enumerate)
+    def meth2(cls):
+        pass
+
+
+@decorator
+@argued_decorator(int, 100, 'test', enumerate)
+def function():
+    pass
